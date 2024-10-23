25addce3b311e6b2c30e6a7f3c6f8e486dbc0cc3
Jonathan Gamboa <jongamboa@gmail.com>
2024-10-23 14:50:10 -0600

    Modified ignition module file

--- smart-contracts/ignition/modules/CombinedModule.js
+++ smart-contracts/ignition/modules/CombinedModule.js
@@ -5,14 +5,14 @@ const CombinedModule = buildModule("CombinedModule", (m) => {
   const reputationNFT = m.contract("ReputationNFT", [], { id: "ReputationNFT" });
 
   // Step 2: Deploy the UserRegistry contract
-  const userRegistry = m.contract("UserRegistry", [reputationNFT], { id: "UserRegistry" });
+  const userRegistry = m.contract("UserRegistry", [], { id: "UserRegistry" });
 
   //Step 3: Deploy the JobListing contract
   const jobListing = m.contract("JobListing", [userRegistry], { id: "JobListing" });
 
   //Step 4: Deploy the HiringContract
-  const paymentTokenAddress = "0x1CBd3b2770909D4e10f157cABC84C7264073C9Ec"; // Replace this with your existing token address
-  const hiringContract = m.contract("HiringContract", [userRegistry,jobListing, paymentTokenAddress], { id: "HiringContract" });
+  //const paymentTokenAddress = "0x1CBd3b2770909D4e10f157cABC84C7264073C9Ec";
+  const hiringContract = m.contract("HiringContract", [userRegistry,jobListing, reputationNFT], { id: "HiringContract" });
 
   return { reputationNFT, userRegistry, jobListing, hiringContract };
 });
