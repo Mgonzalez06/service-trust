94d000a6b03be707572d29912c3a3fb13725dc12
Jonathan Gamboa <jongamboa@gmail.com>
2024-10-24 00:42:32 -0600

    Deleted comments

--- smart-contracts/scripts/interact.js
+++ smart-contracts/scripts/interact.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const contractAddress = "0x8E67154c0566e09422B226d190C073bBd0737046"; // Asegúrate de que esta sea la dirección correcta
+    const contractAddress = "0x8E67154c0566e09422B226d190C073bBd0737046"; 
     const UserRegistry = await ethers.getContractFactory("UserRegistry");
     const userRegistry = UserRegistry.attach(contractAddress);
 
@@ -11,11 +11,11 @@ async function main() {
     const country = "Costa Rica";
     const city = "San José";
     const nationality = "Costarricense";
-    const birthdate = 1234567890; // Asegúrate de que sea un timestamp válido
-    const email = "juan.perez@example.com"; // Asegúrate de que este email no esté ya en uso
-    const passwordHash = "0x2748f3f7d035b771b3f93283c2b26bb177f7e6fd7a9f6bcd1f363909b2d5b952"; // Reemplaza con el hash real
+    const birthdate = 1234567890; 
+    const email = "juan.perez@example.com"; 
+    const passwordHash = "0x2748f3f7d035b771b3f93283c2b26bb177f7e6fd7a9f6bcd1f363909b2d5b952"; 
     const phone = "12345678";
-    const walletAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; // Dirección de la billetera del usuario
+    const walletAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; 
 
     try {
         const tx = await userRegistry.registerUser(
@@ -27,9 +27,9 @@ async function main() {
             birthdate,
             email,
             phone,
-            "Descripción", // Asegúrate de que esto sea un string válido
-            walletAddress, // El walletAddress debe ir entre comillas
-            passwordHash // Asegúrate de que esto sea un string
+            "Descripción", 
+            walletAddress, 
+            passwordHash 
         );
         
         await tx.wait();
