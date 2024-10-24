ad791a896a1984b3931b29bf7ef5c4cc6ff7aac1
Jonathan Gamboa <jongamboa@gmail.com>
2024-10-23 19:56:36 -0600

    configuration of config file to deploy to base sepolia

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,7 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { vars } = require("hardhat/config");
+
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  
   defaultNetwork: "localhost",  // Set the default network to localhost
+  networks: {
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+     "baseSepolia": ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+         apiURL: "https://api-sepolia.basescan.org/api",
+         browserURL: "https://sepolia.basescan.org"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false,
+  },
 };
